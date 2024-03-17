0067efe3cc7ea224c05c2720786df251d25ef94b
nakamura_ryuki <>
2024-03-17 19:15:02 +0900

    自動ログイン実装中

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # launcher_for_web
+##usernameとpassword
+###mac
+`
+export CHATGPT_USERNAME='your_username'
+export CHATGPT_PASSWORD='your_password'
+`
+###windows
+`
+set CHATGPT_USERNAME=your_username
+set CHATGPT_PASSWORD=your_password
+`
--- app.py
+++ app.py
@@ -1,50 +1,84 @@
-import tkinter as tk
-from tkinter import messagebox
+import sys
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 import webbrowser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import os
 
-class App(tk.Tk):
+print(os.environ.get('ChatGPT_USERNAME'))
+
+# WebDriverのパスを指定（このパスを実際のchromedriverの場所に置き換えてください）
+driver_path = '/Users/user/Desktop/project/launcher_web/chromedriver'
+
+service = Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://chat.openai.com/")
+
+driver.implicitly_wait(10)
+
+# 現在のページのURLを取得
+current_url = driver.current_url
+print("現在のURL:", current_url)
+
+driver.quit()
+
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.title("WebサイトLauncher")
-        self.geometry("300x100")
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.create_tray_icon()
-        self.tray_icon.run()
+        self.initUI()
 
-    def create_tray_icon(self):
-        self.tray_icon = TrayIcon(self)
+    def initUI(self):
+        self.setWindowTitle('Tray Icon Window')
+        self.setGeometry(100, 100, 640, 480)
+        self.setWindowIcon(QIcon("images/icon/claude_icon.png"))
+        self.label = QLabel("Hello, PyQt5!", self)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.center()
 
-    def on_closing(self):
-        if messagebox.askyesno("確認", "アプリケーションを終了しますか?"):
-            self.tray_icon.stop()
-            self.quit()
+    def center(self):
+        # ディスプレイの解像度を取得
+        screen = QApplication.primaryScreen().geometry()
+        screenWidth = screen.width()
+        screenHeight = screen.height()
 
-class TrayIcon(tk.Label):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
-        self.icon = tk.PhotoImage(file="images/icon/claude_icon.png")
-        self.config(image=self.icon)
-        self.bind("<Button-1>", self.open_website)
-        self.master.overrideredirect(True)
-        self.master.withdraw()
-        self.master.after(300, self.place_window)
-        
+        # ウィンドウのサイズを取得
+        windowWidth = self.frameSize().width()
+        windowHeight = self.frameSize().height()
 
-    def place_window(self):
-        x = self.master.winfo_pointerx() - 16
-        y = self.master.winfo_pointery() - 16
-        self.master.geometry(f"+{x}+{y}")
-        self.lift()
-        self.master.after(10, self.place_window)
+        # ウィンドウを画面の右の真ん中に配置
+        self.move(screenWidth - windowWidth, (screenHeight - windowHeight) // 2)
 
-    def open_website(self, event):
-        webbrowser.open("https://claude.ai/chat/")
+class TrayIconApp:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.window = MainWindow()
+        self.tray_icon = QSystemTrayIcon(QIcon("images/icon/claude_icon.png"), self.app)
+        self.setup_tray_icon()
 
-    def stop(self):
+    def setup_tray_icon(self):
+        menu = QMenu()
+        open_website_action = menu.addAction("ウェブサイトを開く")
+        exit_action = menu.addAction("終了")
 
-        self.master.destroy()
+        open_website_action.triggered.connect(self.open_website)
+        exit_action.triggered.connect(self.exit_app)
+
+        self.tray_icon.setContextMenu(menu)
+        self.tray_icon.show()
+
+    def open_website(self):
+        webbrowser.open("https://chat.openai.com/")
+
+    def exit_app(self):
+        QApplication.quit()
+
+    def run(self):
+        sys.exit(self.app.exec_())
 
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    tray_icon_app = TrayIconApp()
+    tray_icon_app.run()
